5e580a54f7ad7cb65343a24498304d9448891633
Bela Ban <belaban@mailbox.org>
2023-12-19 09:15:12 +0100

    Null send_stream onError() and recreate in _connect()

--- src/main/java/org/jgroups/rolling_upgrades/UpgradeClient.java
+++ src/main/java/org/jgroups/rolling_upgrades/UpgradeClient.java
@@ -6,14 +6,17 @@ import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import io.netty.handler.ssl.SslContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jgroups.rolling_upgrades.UpgradeServiceGrpc.UpgradeServiceStub;
 
+import javax.annotation.concurrent.GuardedBy;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
-import java.util.logging.Logger;
 
 import static org.jgroups.rolling_upgrades.ConnectionStatus.State.*;
 
@@ -29,7 +32,7 @@ public class UpgradeClient implements StreamObserver<Response> {
     protected int                          server_port=50051;
     protected String                       server_cert;
     protected ManagedChannel               channel;
-    protected UpgradeServiceGrpc.UpgradeServiceStub asyncStub;
+    protected UpgradeServiceStub           asyncStub;
     // protected UpgradeServiceBlockingStub   syncStub; // we can have both a sync/async stub; they use the same channel
     protected StreamObserver<Request>      send_stream;
     protected final Set<Consumer<View>>    view_handlers=new HashSet<>();
@@ -39,26 +42,28 @@ public class UpgradeClient implements StreamObserver<Response> {
     protected long                         reconnect_interval=3000; // in ms
     protected Runner                       reconnector;
     protected Runnable                     reconnect_function;
-    protected static final Logger          log=Logger.getLogger(UpgradeClient.class.getSimpleName());
+    protected Logger                       log;
 
-    public String        getServerAddress()                                  {return server_address;}
-    public UpgradeClient setServerAddress(String a)                          {server_address=a; return this;}
-    public int           getServerPort()                                     {return server_port;}
-    public UpgradeClient setServerPort(int p)                                {server_port=p; return this;}
-    public String        getServerCert()                                     {return server_cert;}
-    public UpgradeClient setServerCert(String c)                             {server_cert=c; return this;}
-    public boolean       isConnected()                                       {return state.isState(ConnectionStatus.State.connected);}
+    public String           serverAddress()                                  {return server_address;}
+    public UpgradeClient    serverAddress(String a)                          {server_address=a; return this;}
+    public int              serverPort()                                     {return server_port;}
+    public UpgradeClient    serverPort(int p)                                {server_port=p; return this;}
+    public String           serverCert()                                     {return server_cert;}
+    public UpgradeClient    serverCert(String c)                             {server_cert=c; return this;}
+    public boolean          connected()                                       {return state.isState(connected);}
     public ConnectionStatus state()                                          {return state;}
-    public UpgradeClient setReconnectionFunction(Runnable f)                 {reconnect_function=f; return this;}
-    public long          getReconnectInterval()                              {return reconnect_interval;}
-    public UpgradeClient setReconnectInterval(long i)                        {reconnect_interval=i; return this;}
-    public UpgradeClient addViewHandler(Consumer<View> h)                    {view_handlers.add(h); return this;}
-    public UpgradeClient removeViewHandler(Consumer<View> h)                 {view_handlers.remove(h); return this;}
-    public UpgradeClient addMessageHandler(Consumer<Message> h)              {message_handlers.add(h); return this;}
-    public UpgradeClient removeMessageHandler(Consumer<Message> h)           {message_handlers.remove(h); return this;}
-    public UpgradeClient setViewResponseHandler(Consumer<GetViewResponse> h) {this.view_rsp_handler=h; return this;}
-    public Consumer<GetViewResponse> getViewResponseHandler()                {return view_rsp_handler;}
-    public boolean       reconnectorRunning()                                {return reconnector.isRunning();}
+    public UpgradeClient    reconnectionFunction(Runnable f)                 {reconnect_function=f; return this;}
+    public long             reconnectInterval()                              {return reconnect_interval;}
+    public UpgradeClient    reconnectInterval(long i)                        {reconnect_interval=i; return this;}
+    public UpgradeClient    addViewHandler(Consumer<View> h)                 {view_handlers.add(h); return this;}
+    public UpgradeClient    removeViewHandler(Consumer<View> h)              {view_handlers.remove(h); return this;}
+    public UpgradeClient    addMessageHandler(Consumer<Message> h)           {message_handlers.add(h); return this;}
+    public UpgradeClient    removeMessageHandler(Consumer<Message> h)        {message_handlers.remove(h); return this;}
+    public UpgradeClient    viewResponseHandler(Consumer<GetViewResponse> h) {this.view_rsp_handler=h; return this;}
+    public Consumer<GetViewResponse> viewResponseHandler()                   {return view_rsp_handler;}
+    public boolean          reconnectorRunning()                             {return reconnector.isRunning();}
+    public Logger           log()                                            {return log;}
+    public UpgradeClient    log(Logger l)                                    {log=l; return this;}
 
 
 
@@ -66,9 +71,12 @@ public class UpgradeClient implements StreamObserver<Response> {
         InputStream server_cert_stream=null;
         SslContext  ctx=null;
 
+        if(log == null)
+            log=LogManager.getFormatterLogger(getClass());
+
         if(server_cert != null && !server_cert.trim().isEmpty()) {
             if((server_cert_stream=Utils.getFile(server_cert)) == null)
-                throw new FileNotFoundException(String.format("server certificate (%s) not found", server_cert));
+                throw new FileNotFoundException(String.format("server certificate %s not found", server_cert));
             ctx=GrpcSslContexts.forClient().trustManager(server_cert_stream).build();
         }
         NettyChannelBuilder cb=NettyChannelBuilder.forAddress(server_address, server_port);
@@ -76,32 +84,23 @@ public class UpgradeClient implements StreamObserver<Response> {
             channel=cb.usePlaintext().build();
         else
             channel=cb.sslContext(ctx).build();
-        asyncStub=UpgradeServiceGrpc.newStub(channel);
         // syncStub=UpgradeServiceGrpc.newBlockingStub(channel);
+        asyncStub=UpgradeServiceGrpc.newStub(channel); // .withWaitForReady();
         if(reconnect_function != null)
             reconnector=createReconnector();
         return this;
     }
 
     public UpgradeClient stop() {
-        if(channel != null) {
-            channel.shutdown();
-            try {
-                channel.awaitTermination(30, TimeUnit.SECONDS);
-            }
-            catch(InterruptedException e) {
-            }
+        channel.shutdown();
+        state.setState(disconnecting);
+        try {
+            channel.awaitTermination(30, TimeUnit.SECONDS);
         }
-        return this;
-    }
-
-    public synchronized UpgradeClient registerView(String cluster, View local_view, Address local_addr) {
-        if(state.setState(disconnected, connecting)) {
-            send_stream=asyncStub.connect(this);
-            RegisterView register_req=RegisterView.newBuilder().setClusterName(cluster).setView(local_view)
-              .setLocalAddr(local_addr).build();
-            Request req=Request.newBuilder().setRegisterReq(register_req).build();
-            send_stream.onNext(req);
+        catch(InterruptedException e) {
+        }
+        finally {
+            state.setState(disconnected);
         }
         return this;
     }
@@ -109,37 +108,40 @@ public class UpgradeClient implements StreamObserver<Response> {
     public synchronized UpgradeClient getViewFromServer(String cluster) {
         GetViewRequest gv=GetViewRequest.newBuilder().setClusterName(cluster).build();
         Request req=Request.newBuilder().setGetViewReq(gv).build();
-        send_stream.onNext(req);
+        send(req);
         return this;
     }
 
-    public synchronized UpgradeClient connect(String cluster, Address local_addr) {
-        if(state.setState(disconnected, connecting)) {
-            send_stream=asyncStub.connect(this);
-            JoinRequest join_req=JoinRequest.newBuilder().setAddress(local_addr).setClusterName(cluster).build();
-            Request req=Request.newBuilder().setJoinReq(join_req).build();
-            send_stream.onNext(req);
-        }
-        return this;
+    public UpgradeClient registerView(String cluster, View local_view, Address local_addr) {
+        RegisterView register_req=RegisterView.newBuilder().setClusterName(cluster).setView(local_view)
+          .setLocalAddr(local_addr).build();
+        Request req=Request.newBuilder().setRegisterReq(register_req).build();
+        return _connect(req);
     }
 
-    public synchronized UpgradeClient disconnect(String cluster, Address local_addr) {
-        if(send_stream != null) {
-            if(local_addr != null && cluster != null) {
-                LeaveRequest leave_req=LeaveRequest.newBuilder().setClusterName(cluster).setLeaver(local_addr).build();
-                Request request=Request.newBuilder().setLeaveReq(leave_req).build();
-                send_stream.onNext(request);
-                state.setState(disconnected);
-            }
-            send_stream.onCompleted();
+    public UpgradeClient connect(String cluster, Address local_addr) {
+        JoinRequest join_req=JoinRequest.newBuilder().setAddress(local_addr).setClusterName(cluster).build();
+        Request req=Request.newBuilder().setJoinReq(join_req).build();
+        return _connect(req);
+    }
+
+    public UpgradeClient disconnect(String cluster, Address local_addr) {
+        if(local_addr == null || cluster == null)
+            return this;
+        LeaveRequest leave_req=LeaveRequest.newBuilder().setClusterName(cluster).setLeaver(local_addr).build();
+        Request request=Request.newBuilder().setLeaveReq(leave_req).build();
+        synchronized(this) {
+            send(request);
+            state.setState(disconnected);
+            if(send_stream != null)
+                send_stream.onCompleted();
         }
         return this;
     }
 
     public synchronized UpgradeClient send(Request req) {
-        if(state.isStateOneOf(disconnected, disconnecting))
-            throw new IllegalStateException(String.format("not connected to %s:%d", server_address, server_port));
-        send_stream.onNext(req);
+        if(checkSendStream())
+            send_stream.onNext(req);
         return this;
     }
 
@@ -153,7 +155,7 @@ public class UpgradeClient implements StreamObserver<Response> {
             return;
         }
         if(rsp.hasRegViewOk()) {
-            state.setState(ConnectionStatus.State.connected);
+            state.setState(connected);
             return;
         }
         if(rsp.hasGetViewRsp()) {
@@ -164,16 +166,36 @@ public class UpgradeClient implements StreamObserver<Response> {
         throw new IllegalStateException(String.format("response is illegal: %s", rsp));
     }
 
-    public void onError(Throwable t) {
-        if(state.isState(ConnectionStatus.State.connected))
-            log.warning(String.format("exception from server: %s (%s)", t, t.getCause()));
-        state.setState(disconnected);
+    public synchronized void onError(Throwable t) {
+        if(state.isState(connected))
+            log.warn(String.format("exception from server: %s (%s)", t, t.getCause()));
+        send_stream=null;
+        state.setState(reconnecting);
         startReconnector();
     }
 
-    public void onCompleted() {
+    public synchronized void onCompleted() {
+        send_stream=null;
+        state.setState(disconnected);
     }
 
+    protected synchronized UpgradeClient _connect(Request req) {
+        if(send_stream == null) {
+            state.setState(connecting);
+            send_stream=asyncStub.connect(this);
+        }
+        send_stream.onNext(req);
+        return this;
+    }
+
+    @GuardedBy("this")
+    protected boolean checkSendStream() {
+        if(send_stream == null) {
+            log.error("send stream is null, cannot send message");
+            return false;
+        }
+        return true;
+    }
 
     protected void handleMessage(Message msg) {
         for(Consumer<Message> c: message_handlers)
@@ -181,23 +203,22 @@ public class UpgradeClient implements StreamObserver<Response> {
     }
 
     protected void handleView(View view) {
-        state.setState(ConnectionStatus.State.connected);
+        state.setState(connected);
         stopReconnector();
         for(Consumer<View> c: view_handlers)
             c.accept(view);
     }
 
     protected synchronized Runner createReconnector() {
-        return new Runner("client-reconnector",
-                          () -> {
-                              reconnect_function.run();
-                              Utils.sleep(reconnect_interval);
-                          },null);
+        return new Runner("client-reconnector", () -> {
+            Utils.sleep(reconnect_interval);
+            reconnect_function.run();
+        }, null);
     }
 
     protected synchronized UpgradeClient startReconnector() {
         if(reconnector != null && !reconnector.isRunning()) {
-            log.fine("starting reconnector");
+            log.debug("starting reconnector");
             reconnector.start();
         }
         return this;
@@ -205,12 +226,11 @@ public class UpgradeClient implements StreamObserver<Response> {
 
     protected synchronized UpgradeClient stopReconnector() {
         if(reconnector != null && reconnector.isRunning()) {
-            log.fine("stopping reconnector");
+            log.debug("stopping reconnector");
             reconnector.stop();
         }
         return this;
     }
-
 
     public static void main(String[] args) throws Exception {
         UpgradeClient client=new UpgradeClient()
